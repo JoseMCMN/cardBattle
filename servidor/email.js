43c5fb666d3216d80ef287ef3838f56569eebf35
JOSE MARTINEZ NAVARRO <Jose.Martinez10@alu.uclm.es>
2018-12-01 11:07:36 +0100

    actualizacion gitignore

--- servidor/email.js
+++ servidor/email.js
@@ -1,4 +1,4 @@
-var sendgrid = require("sendgrid")("JoseMCMN","mypassword236");
+var sendgrid = require("sendgrid")("XXXXXXX","XXXXXXX");
 
 //var url="https://cardbattlegame.herokuapp.com/";
 var url="http://127.0.0.1:5000/";
@@ -9,8 +9,8 @@ module.exports.enviarEmail=function(direccion,key,msg){
     var email = new sendgrid.Email();
     email.addTo(direccion);
     email.setFrom('josemcmn@gmail.com');
-    email.setSubject('confirmar cuenta');
-    email.setHtml('<h3>Bienvenido a BattleCards</h3><p><a href="'+url+'confirmarUsuario/'+direccion+'/'+key+'">'+msg+'</a></p>');
+    email.setSubject('cardBattle: Confirmar cuenta');
+    email.setHtml('<h3>Bienvenido a cardBattle</h3><p><a href="'+url+'confirmarUsuario/'+direccion+'/'+key+'">'+msg+'</a></p>');
 
     sendgrid.send(email);    
 }
